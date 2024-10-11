76c6f6a540a0fac28bd107e2174b819e9296757f
Abhishek Aggarwal <abhishekmonu2000@gmail.com>
2024-10-11 16:45:22 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,14 +13,18 @@ function Navbar() {
   return (
     <nav className="p-4 md:p-6 shadow-md bg-gray-900 text-white">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+        
         <a href="#" className="text-xl font-bold mb-4 md:mb-0">
         MaskMyFeed
         </a>
+
+        <div className='flex gap-5 '>
         {session ? (
           <>
-            <span className="mr-4">
+            <span className="mr-4 mt-2">
               Welcome, {user.username || user.email}
             </span>
+            
             <Button onClick={() => signOut()} className="w-full md:w-auto bg-slate-100 text-black" variant='outline'>
               Logout
             </Button>
@@ -30,6 +34,11 @@ function Navbar() {
             <Button className="w-full md:w-auto bg-slate-100 text-black" variant={'outline'}>Login</Button>
           </Link>
         )}
+        <Link href="https://mask-my-feed-g1fwv341t-abhishekaggarawals-projects.vercel.app/u/Abhishek">
+            <Button className="w-full md:w-auto bg-slate-100 text-black" variant={'outline'}>Give feedback to Creater</Button>
+          </Link>
+        </div>
+        
       </div>
     </nav>
   );
